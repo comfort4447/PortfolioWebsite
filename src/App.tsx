9c86479d28a13dc90915f0d1bccbee34689bb137
ABISOLA TAYO <tayoabisola44@gmail.com>
2023-07-10 10:28:47 +0100

    homepage

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,14 @@ import History from './components/History/History';
 import CreateAccount from './components/NewAccount/CreateAccount';
 import ListAccounts from './components/ListAccounts/ListAccounts';
 import NewAccount from './components/NewAccount/NewAccount';
+import HomePage from './components/Landing Page/HomePage';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path='/' element={<HomePage />} />
         <Route path='/' element={<SignUp />} />
         <Route path='/signin' element={<Login />} />
         <Route path='/dashboard' element={<Dashboard />}/>
